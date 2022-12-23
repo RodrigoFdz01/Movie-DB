f4850ed50cfe451499c0eee51ce2e9f04b4d2db6
RodrigoFdz01 <rodrigof3012@gmail.com>
2022-12-22 23:31:14 -0300

    some files

--- Movie DB/app.js
+++ Movie DB/app.js
@@ -1,4 +1,16 @@
-document.addEventListener("DOMContentLoaded", cargarPeliculas);
+//document.addEventListener("DOMContentLoaded", cargarPeliculas);
+const boton = document.getElementById("#boton");
+const pelicula = document.querySelector("pelicula").value;
+
+boton.addEventListener("click", function () {
+  buscarPelicula();
+  pintarPelicula();
+});
+
+function buscarPelicula(e) {
+  e.preventDefault();
+  consultarApi("pelicula");
+}
 
 //https://api.themoviedb.org/3/trending/movie/week?api_key=79f0e639de5e3a1e7b6bb5f9122307c0
 
@@ -14,6 +26,7 @@ async function cargarPeliculas() {
     console.log(error);
   }
 }
+//cargarPeliculas();
 
 const pintarPeliculas = (pelicula) => {
   pelicula.forEach((pel) => {
@@ -31,13 +44,26 @@ const pintarPeliculas = (pelicula) => {
   });
 };
 
-async function verPelicula(id) {
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=79f0e639de5e3a1e7b6bb5f9122307c0&language=es-ES`;
-  try {
-    const respuesta = await fetch(url);
-    const resultado = await respuesta.json();
-    mostrarPelicula(resultado);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function verPelicula(id) {
+//   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=79f0e639de5e3a1e7b6bb5f9122307c0&language=es-ES`;
+//   try {
+//     const respuesta = await fetch(url);
+//     const resultado = await respuesta.json();
+//     mostrarPelicula(resultado);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+const pintarPelicula = (pelicula) => {
+  const { backdrop_path, title, release_date } = pelicula;
+  //console.log(title);
+
+  const urlImagen = `https://image.tmdb.org/t/p/w500/${backdrop_path}`;
+  result.innerHTML = `
+    <div class="movie">
+    <img src = "${urlImagen}" class="img-fluid mx-5" alt = "movie poster">
+        <h3 class = "movie-title text-center mt-2">${title}</h3>  
+        <li class = "released">Fecha de lanzamiento: ${release_date}</li>
+    </div> `;
+};
